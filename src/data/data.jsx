c8e520339a9af42f6123f328ecdeb42b2f13ba8f
Codehub004 <135439341+Codehub004@users.noreply.github.com>
2023-09-14 15:08:38 +0530

    Add files via upload

---
+++ src/data/data.jsx
@@ -0,0 +1,28 @@
+import Hoobank from '../assets/projects/hoobank.png'
+import Ecommerce from '../assets/projects/ecommerce.png'
+import Contactmangapp from '../assets/projects/contactmangapp.png'
+
+export const data=[
+    {
+        id:1,
+        name:"Hoobank UI/UX banking application",
+        image:Hoobank,
+        github:"https://github.com/Codehub004/bank_modern_app.git",
+        live:" https://bank-modern-app-chi.vercel.app/",
+    },
+    {
+        id:2,
+        name:"Ecommerce Website",
+        image:Ecommerce,
+        github:"https://github.com/Codehub004/eshop.git",
+        live:"",
+    },
+    {
+        id:3,
+        name:"Contact management app",
+        image:Contactmangapp,
+        github:"https://github.com/Codehub004/codingtask.git",
+        live:" https://codingtask-nine.vercel.app/",
+    },
+    
+]
