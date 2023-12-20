7b238b995b43b1d650bf33f3f27eca999b1c5075
Codehub004 <135439341+Codehub004@users.noreply.github.com>
2023-12-20 16:08:01 +0530

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -125,7 +125,7 @@ const Navbar = () => {
           <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#565f69]'>
             <a
               className='flex justify-between items-center w-full text-gray-300'
-              href='https://drive.google.com/file/d/1n5cnTUTzZ9_XTPeH3t6vuKPztX8xLAV4/view?usp=drive_link'
+              href='https://drive.google.com/file/d/15vFXVy_abWcJrUuzgpdUpwn6HETW5aTu/view?usp=drive_link'
             >
               Resume <BsFillPersonLinesFill size={30} />
             </a>
