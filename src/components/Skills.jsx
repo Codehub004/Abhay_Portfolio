792f6acf5e803fd8d775f3060df5592a68dfc138
Codehub004 <135439341+Codehub004@users.noreply.github.com>
2024-01-15 01:17:34 +0530

    Updated

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -4,7 +4,7 @@ import HTML from '../assets/html.png';
 import CSS from '../assets/css.png';
 import JavaScript from '../assets/javascript.png';
 import ReactImg from '../assets/react.png';
-import Node from '../assets/node.png';
+import SQL from '../assets/sql.png';
 import FireBase from '../assets/firebase.png';
 import GitHub from '../assets/github.png';
 import Tailwind from '../assets/tailwind.png';
@@ -45,6 +45,10 @@ const Skills = () => {
               <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
                   <img className='w-20 mx-auto' src={Tailwind} alt="HTML icon" />
                   <p className='my-4'>TAILWIND</p>
+              </div>
+              <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
+                  <img className='w-20 mx-auto' src={SQL} alt="HTML icon" />
+                  <p className='my-4'>SQl</p>
               </div>
               <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
                   <img className='w-20 mx-auto' src={FireBase} alt="HTML icon" />
--- src/data/data.jsx
+++ src/data/data.jsx
@@ -1,28 +1,28 @@
-import Hoobank from '../assets/projects/hoobank.png'
+import Figma from '../assets/projects/figma.png'
 import Ecommerce from '../assets/projects/ecommerce.png'
 import Contactmangapp from '../assets/projects/contactmangapp.png'
 
 export const data=[
     {
         id:1,
-        name:"Hoobank UI/UX banking application",
-        image:Hoobank,
-        github:"https://github.com/Codehub004/bank_modern_app.git",
-        live:" https://bank-modern-app-chi.vercel.app/",
-    },
-    {
-        id:2,
         name:"Ecommerce Website",
         image:Ecommerce,
         github:"https://github.com/Codehub004/ecommerce_website.git",
         live:"https://ecommerce-website-omega-two.vercel.app/",
     },
     {
-        id:3,
+        id:2,
         name:"Contact management app & Chart",
         image:Contactmangapp,
         github:"https://github.com/Codehub004/codingtask.git",
         live:" https://codingtask-nine.vercel.app/",
+    },
+    {
+        id:3,
+        name:"Figma Design",
+        image:Figma,
+        github:"https://github.com/Codehub004/figma_design.git",
+        live:" https://figma-design-psi.vercel.app/",
     },
     
 ]
