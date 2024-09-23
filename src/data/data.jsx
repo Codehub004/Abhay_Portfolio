02e5dd247eb3d0ce0cfe2756b2797d2c6e4fc438
Abhay Upadhyay <135439341+Codehub004@users.noreply.github.com>
2024-09-23 12:15:51 +0530

    Add files via upload

---
+++ src/data/data.jsx
@@ -0,0 +1,40 @@
+import Figma from '../assets/projects/figma.png'
+import Ecommerce from '../assets/projects/ecommerce.jpeg'
+import Contactmangapp from '../assets/projects/contactmangapp.png'
+import Blog from '../assets/projects/blog.jpg'
+
+export const data=[
+   
+    {
+        id:1,
+        name:"Full Stack Blog App",
+        image:Blog,
+        github:"https://github.com/Codehub004/Full_Stack_Blog_App.git",
+        // live:"https://ecommerce-website-omega-two.vercel.app/",
+    },
+
+    {
+        id:2,
+        name:" React Ecommerce Website",
+        image:Ecommerce,
+        github:"https://github.com/Codehub004/ecommerce_website.git",
+        live:"https://ecommerce-website-omega-two.vercel.app/",
+    },
+ 
+    {
+        id:3,
+        name:"Figma Design html/css",
+        image:Figma,
+        github:"https://github.com/Codehub004/figma_design.git",
+        live:" https://figma-design-psi.vercel.app/",
+    },
+   
+    {
+        id:4,
+        name:"Contact management app & Chart",
+        image:Contactmangapp,
+        github:"https://github.com/Codehub004/codingtask.git",
+        live:" https://codingtask-nine.vercel.app/",
+    },
+   
+]
